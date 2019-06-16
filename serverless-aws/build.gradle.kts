ecc4ebfe44e346d79e95937918806175dd2838c3
Sergey Okatov <sokatov@gmail.com>
2019-06-16 13:21:00 +0500

    All works except lambda

--- serverless-aws/build.gradle.kts
+++ serverless-aws/build.gradle.kts
@@ -6,6 +6,32 @@ plugins {
 }
 
 nodeSetup()
+//data class TaskSettings(
+//    val stage: String = "dev",
+//    val taskName: String = "task",
+//    val srcProject: Project
+//)
+//
+//fun Project.slsTask(settings: TaskSettings) {
+//    this.tasks {
+//        val copyTask = task<Sync>("copy${settings.taskName}") {
+//            from(settings.srcProject.configurations.getByName("serverlessArtifacts").artifacts.files)
+//            into("$buildDir/web-static")
+//        }
+//        val uploadTask = task<Exec>("uploadStatic") {
+//            dependsOn(copyTask)
+//            commandLine = listOf(
+//                "aws",
+//                "s3",
+//                "cp",
+//                "$buildDir/web-static",
+//                "s3://$projectStage.$projectDomain-static/",
+//                "--recursive",
+//                "--include", "*"
+//            )
+//        }
+//    }
+//}
 
 tasks {
 
@@ -14,86 +40,104 @@ tasks {
     val projectService: String by project
     val domainCertificate: String by project
 
+    // Basic setup for API Gateway
+    val deployBase = task<YarnTask>("deployBase") {
+        val workDir = "$projectDir/sls-api-gw"
+        dependsOn("yarn_install")
+
+        inputs.files(fileTree("node_modules"))
+        inputs.file("$workDir/serverless.yml")
+        inputs.file("$workDir/package.json")
+        setWorkingDir(File(workDir))
+
+        args = listOf(
+            "run", "deploy",
+            "--no-confirm",
+            "--stage", projectStage,
+            "--domain", projectDomain,
+            "--service", projectService
+        )
+    }
+
+    // Tasks for Root static web-site
     val copyStatic = task<Sync>("copyStatic") {
         from(project(":front-static").configurations.getByName("serverlessArtifacts").artifacts.files)
         into("$buildDir/web-static")
     }
+    val deployStatic = task<YarnTask>("deployStatic") {
+        val workDir = "$projectDir/sls-front-static"
+        dependsOn("yarn_install", copyStatic)
+
+        inputs.files(fileTree("node_modules"))
+        inputs.file("$workDir/serverless.yml")
+        inputs.file("$workDir/package.json")
+        setWorkingDir(File(workDir))
+
+        args = listOf(
+            "run", "deploy",
+            "--no-confirm",
+            "--stage", projectStage,
+            "--domain", projectDomain,
+            "--service", projectService
+        )
+    }
     val uploadStatic = task<Exec>("uploadStatic") {
-        dependsOn(copyStatic)
+        val publishDir = "$buildDir/web-static"
+        dependsOn(deployStatic)
+        inputs.files(fileTree(publishDir))
         commandLine = listOf(
             "aws",
             "s3",
             "cp",
-            "$buildDir/web-static",
+            publishDir,
             "s3://$projectStage.$projectDomain-static/",
             "--recursive",
             "--include", "*"
         )
     }
 
-    val copySpa = task<Sync>("copyAngular") {
-        dependsOn(copyStatic)
+    // Tasks for subpath SPA web-site
+    val copySpa = task<Sync>("copySpa") {
         from(project(":front-angular").configurations.getByName("serverlessArtifacts").artifacts.files)
         into("$buildDir/web-spa")
     }
+    val deploySpa = task<YarnTask>("deploySpa") {
+        val workDir = "$projectDir/sls-front-angular"
+        dependsOn("yarn_install", copySpa)
+
+        inputs.files(fileTree("node_modules"))
+        inputs.file("$workDir/serverless.yml")
+        inputs.file("$workDir/package.json")
+        setWorkingDir(File(workDir))
+
+        args = listOf(
+            "run", "deploy",
+            "--no-confirm",
+            "--stage", projectStage,
+            "--domain", projectDomain,
+            "--service", projectService
+        )
+    }
     val uploadSpa = task<Exec>("uploadSpa") {
-        dependsOn(copySpa)
+        val publishDir = "$buildDir/web-spa"
+        dependsOn(deploySpa)
+        inputs.files(fileTree(publishDir))
         commandLine = listOf(
             "aws",
             "s3",
             "cp",
-            "$buildDir/web-spa",
+            publishDir,
             "s3://$projectStage.$projectDomain-spa/",
             "--recursive",
             "--include", "*"
         )
     }
 
+    // Tasks for Lambda functions
     val jarDynamic = project(":front-dynamic").configurations.getByName("serverlessArtifacts").artifacts.files
     val copyDynamic = task<Sync>("copyDynamic") {
         from(jarDynamic)
         into("$buildDir/libs")
-    }
-
-    val copyAll = create("copyAll") {
-        dependsOn(
-            copyStatic,
-            copySpa,
-            copyDynamic
-        )
-    }
-
-    val slsArgs = arrayOf(
-        "--no-confirm",
-        "--stage", projectStage,
-        "--domain", projectDomain,
-        "--domain-cert", domainCertificate,
-        "--jar", jarDynamic.joinToString(","),
-        "--service", projectService
-    )
-
-    val ngBuild = task<YarnTask>("ngBuild") {
-        dependsOn("yarn_install", copyAll)
-
-        inputs.files(fileTree("node_modules"))
-        inputs.file("serverless.yml")
-        inputs.file("package.json")
-
-//        outputs.dir("dist")
-
-        args = listOf("run", "pack", *slsArgs)
-    }
-
-    val ngDeploy = task<YarnTask>("ngDeploy") {
-        dependsOn("yarn_install", copyAll)
-
-        inputs.files(fileTree("node_modules"))
-        inputs.file("serverless.yml")
-        inputs.file("package.json")
-
-        setEnvironment(mapOf("SLS_DEBUG" to "*"))
-
-        args = listOf("run", "deploy", *slsArgs)
     }
 
     clean.get().doLast {
@@ -103,17 +147,17 @@ tasks {
     }
 
     build.get().dependsOn(
-        ngBuild,
         ":front-static:build",
         ":front-angular:build",
         ":front-dynamic:build"
     )
     create("deploy") {
-        dependsOn(ngDeploy)
-        dependsOn(":front-static:build")
-        dependsOn(":front-angular:build")
-        dependsOn(":front-dynamic:build")
-        dependsOn(uploadStatic, uploadSpa)
+        group = "build"
+        dependsOn(
+            deployBase,
+            uploadStatic,
+            uploadSpa
+        )
         group = "build"
     }
 
